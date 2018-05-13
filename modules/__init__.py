00dc399a2cf5b51ca31554bf3a1b07e22f12ad2f
Ping-Lin <billy3962@hotmail.com>
2018-05-13 18:01:43 +0800

    change print to logging

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,3 +4,12 @@ Stock Analysis Document.
 
 ..  moduleauthor:: Ping-Lin <billy3962@hotmail.com>
 """
+
+import logging
+DEBUG = False
+
+if DEBUG:
+    level = logging.DEBUG
+else:
+    level = logging.INFO
+logging.basicConfig(level=level)
